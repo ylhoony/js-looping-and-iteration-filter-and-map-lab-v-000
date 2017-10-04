5554ad68cd989dba7d19a6e51aa1dd56bec5538e
Hoon Lee <yl.hoony@gmail.com>
2017-10-04 17:51:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // Code your solution here:
- 
+
 function driversWithRevenueOver(collection, num) {
   return collection.filter(function(driver) {
     return driver.revenue > num;
   });
+}
+
+function driverNamesWithRevenueOver(collection, num) {
+  const newCollection = [];
+  newcollection.push(collection.filter(function(driver) {
+    return driver.revenue > num;
+  }));
 }
